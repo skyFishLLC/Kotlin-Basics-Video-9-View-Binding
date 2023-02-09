ef3d2cb672353c2b482e3d862aafd72a3d83991d
Tim-Desktop <timothy.fischer@skyfish.io>
2023-02-09 10:40:22 +0530

    Ending Code for Kotlin Basics 9: View Binding, List Indexing

--- app/src/main/java/dev/skyfish/kotlinoverview/MainActivity.kt
+++ app/src/main/java/dev/skyfish/kotlinoverview/MainActivity.kt
@@ -2,14 +2,19 @@ package dev.skyfish.kotlinoverview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import dev.skyfish.kotlinoverview.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         //////////////////////////////////////////////////////////////////
 
         var users = mutableListOf(
@@ -18,6 +23,22 @@ class MainActivity : AppCompatActivity() {
             TrialUserAccount("trialUser","pass123",),
             UserAccount("Jim12","strongPass2345@",true)
         )
+
+        users.add(UserAccount("phil4_32","password12!@",true))
+
+        var currentIndex = 0
+
+        binding.button.setOnClickListener {
+            val current = users[currentIndex]
+            binding.tvUsername.text = current.username
+            binding.tvPassword.text = current.password
+            binding.tvActive.text = if(current.subActive) "Active" else "Not Active"
+            if(currentIndex == users.size - 1) {
+                currentIndex = 0
+            } else {
+                currentIndex++
+            }
+        }
 
 
     }
