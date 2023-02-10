93df6931d3580b41012fd173c7d85edb87ca8388
Tim-Desktop <timothy.fischer@skyfish.io>
2023-02-10 16:08:47 +0530

    Ending Code for Lesson 10: Saving & Restoring Screen State

--- app/src/main/java/dev/skyfish/kotlinoverview/MainActivity.kt
+++ app/src/main/java/dev/skyfish/kotlinoverview/MainActivity.kt
@@ -2,17 +2,18 @@ package dev.skyfish.kotlinoverview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.util.Log
 import dev.skyfish.kotlinoverview.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        logState("onCreate()")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //////////////////////////////////////////////////////////////////
@@ -26,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
         users.add(UserAccount("phil4_32","password12!@",true))
 
-        var currentIndex = 0
+
 
         binding.button.setOnClickListener {
             val current = users[currentIndex]
@@ -39,9 +40,68 @@ class MainActivity : AppCompatActivity() {
                 currentIndex++
             }
         }
-
-
     }
 
+    override fun onStart(){
+        super.onStart()
+        logState("onStart()")
+    }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle){
+        super.onRestoreInstanceState(savedInstanceState)
+        logState("onRestoreInstanceState(..)")
+
+        val username = savedInstanceState.getCharSequence("username")
+        val password = savedInstanceState.getCharSequence("password")
+        val active = savedInstanceState.getCharSequence("active")
+        binding.tvUsername.text = username
+        binding.tvPassword.text = password
+        binding.tvActive.text = active
+        currentIndex = savedInstanceState.getInt("index")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        logState("onResume()")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        logState("onSaveInstanceState(..)")
+        outState.putCharSequence("username",binding.tvUsername.text)
+        outState.putCharSequence("password",binding.tvPassword.text)
+        outState.putCharSequence("active",binding.tvActive.text)
+        outState.putInt("index",currentIndex)
+    }
+
+    override fun onPause(){
+        super.onPause()
+        logState("onPause()")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        logState("onStop()")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        logState("onDestroy()")
+    }
+
+    override fun onRestart(){
+        super.onRestart()
+        logState("onRestart()")
+    }
+
+    fun logState(name: String){
+        Log.i("STATE_DEMO", name)
+    }
 }
+
+
+
+
+
+
+
